ce8bef208648cc94c7fb576dd819409d19e3b81e
kevin amrulloh <kevinamrulloh@gmail.com>
2022-06-22 14:11:53 +0700

    convert json ke css

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,12 +4,22 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 22 Jun 2022 06:55:51 GMT
+// Generated on Wed, 22 Jun 2022 07:11:31 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
+    public static let globalRed0 = #fff6f6
+    public static let globalRed10 = #fce4e4
+    public static let globalRed20 = #f5aeae
+    public static let globalRed30 = #ee7879
+    public static let globalRed40 = #e74243
+    public static let globalRed50 = #e32728
+    public static let globalRed60 = #be1919
+    public static let globalRed70 = #b11718
+    public static let globalRed80 = #760f10
+    public static let globalRed90 = #3b0808
     public static let globalSize32 = 32px
     public static let globalSize48 = 48px
 }
